29140dd4d8edd962e013d25d9fb5c38f9f92e4f2
Max Henkel <max@maxhenkel.de>
2023-04-22 20:29:28 +0200

    Fix auto trader arrows on top of tooltip

---
+++ src/main/java/de/maxhenkel/easyvillagers/gui/ArrowButton.java
@@ -0,0 +1,50 @@
+package de.maxhenkel.easyvillagers.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.maxhenkel.easyvillagers.Main;
+import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+public class ArrowButton extends AbstractButton {
+
+    private static final ResourceLocation ARROW_LEFT = new ResourceLocation(Main.MODID, "textures/gui/arrow_left.png");
+    private static final ResourceLocation ARROW_RIGHT = new ResourceLocation(Main.MODID, "textures/gui/arrow_right.png");
+
+    private final boolean left;
+    private final OnPress onPress;
+
+    public ArrowButton(int x, int y, boolean left, OnPress onPress) {
+        super(x, y, 16, 20, Component.empty());
+        this.left = left;
+        this.onPress = onPress;
+    }
+
+    @Override
+    public void renderButton(PoseStack poseStack, int mouseX, int mouseY, float delta) {
+        super.renderButton(poseStack, mouseX, mouseY, delta);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
+        RenderSystem.setShaderTexture(0, left ? ARROW_LEFT : ARROW_RIGHT);
+        blit(poseStack, x, y + 2, 0, 0, width, height, 16, 16);
+    }
+
+    @Override
+    public void onPress() {
+        onPress.onPress(this);
+    }
+
+    @Override
+    public void updateNarration(NarrationElementOutput elementOutput) {
+
+    }
+
+    @FunctionalInterface
+    public interface OnPress {
+        void onPress(ArrowButton button);
+    }
+
+}
