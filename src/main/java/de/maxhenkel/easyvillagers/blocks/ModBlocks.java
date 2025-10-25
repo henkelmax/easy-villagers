0e1587d0bd0864b53cb2f3734c46c2c9b8677687
Max Henkel <max@maxhenkel.de>
2025-10-25 09:23:38 +0200

    Update NeoForge

--- src/main/java/de/maxhenkel/easyvillagers/blocks/ModBlocks.java
+++ src/main/java/de/maxhenkel/easyvillagers/blocks/ModBlocks.java
@@ -11,14 +11,14 @@ public class ModBlocks {
 
     private static final DeferredRegister.Blocks BLOCK_REGISTER = DeferredRegister.createBlocks(EasyVillagersMod.MODID);
 
-    public static final DeferredHolder<Block, TraderBlock> TRADER = BLOCK_REGISTER.registerBlock("trader", TraderBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, AutoTraderBlock> AUTO_TRADER = BLOCK_REGISTER.registerBlock("auto_trader", AutoTraderBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, FarmerBlock> FARMER = BLOCK_REGISTER.registerBlock("farmer", FarmerBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, BreederBlock> BREEDER = BLOCK_REGISTER.registerBlock("breeder", BreederBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, ConverterBlock> CONVERTER = BLOCK_REGISTER.registerBlock("converter", ConverterBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, IronFarmBlock> IRON_FARM = BLOCK_REGISTER.registerBlock("iron_farm", IronFarmBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, IncubatorBlock> INCUBATOR = BLOCK_REGISTER.registerBlock("incubator", IncubatorBlock::new, BlockBehaviour.Properties.of());
-    public static final DeferredHolder<Block, InventoryViewerBlock> INVENTORY_VIEWER = BLOCK_REGISTER.registerBlock("inventory_viewer", InventoryViewerBlock::new, BlockBehaviour.Properties.of());
+    public static final DeferredHolder<Block, TraderBlock> TRADER = BLOCK_REGISTER.registerBlock("trader", TraderBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, AutoTraderBlock> AUTO_TRADER = BLOCK_REGISTER.registerBlock("auto_trader", AutoTraderBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, FarmerBlock> FARMER = BLOCK_REGISTER.registerBlock("farmer", FarmerBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, BreederBlock> BREEDER = BLOCK_REGISTER.registerBlock("breeder", BreederBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, ConverterBlock> CONVERTER = BLOCK_REGISTER.registerBlock("converter", ConverterBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, IronFarmBlock> IRON_FARM = BLOCK_REGISTER.registerBlock("iron_farm", IronFarmBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, IncubatorBlock> INCUBATOR = BLOCK_REGISTER.registerBlock("incubator", IncubatorBlock::new, BlockBehaviour.Properties::of);
+    public static final DeferredHolder<Block, InventoryViewerBlock> INVENTORY_VIEWER = BLOCK_REGISTER.registerBlock("inventory_viewer", InventoryViewerBlock::new, BlockBehaviour.Properties::of);
 
     public static void init(IEventBus eventBus) {
         BLOCK_REGISTER.register(eventBus);
